da0ada39e02aeb733c5f6a9569e2a6da6b90f478
froggy <chrisdetmering@gmail.com>
2020-11-07 12:53:17 -0800

    Todo app completed

--- todo.js
+++ todo.js
@@ -1,21 +1,72 @@
-  const todosContainer = document.getElementById('todos-container');
-  const todoInput = document.getElementById('todo-input');
-  const todoButton = document.getElementById('add-todo-button');
+const todosContainer = document.getElementById('todos-container');
+const todoInput = document.getElementById('todo-input');
+const todoButton = document.getElementById('add-todo-button');
   
 
-//what do I need? 
-//I need to keep track of 
-//1) Order of todos 
-//2) State of todo 
-//3) If todo is deleted 
-//4) All in localStorage, which does not keep order.
 
-//I could 
-//1) make it an array of todos 
-//2) when a todo is deleted, I iterate through all the current 
-//todos and change their name property to be the order that they currently are in. 
+let addToggleTodoHandler = (todoBody) => { 
+  todoBody.addEventListener('click', () => { 
+    toggleTodoUIStyle(toggleTodoState(todoBody));
+  })
+}
 
-//todos [{todo: 'eat', done: false}, {todo: 'sleep', done: true}]
+
+let toggleTodoState = (todoBody) => { 
+  let id = todoBody.parentElement.getAttribute('name')
+  let todos = JSON.parse(localStorage.getItem('todos'));
+  let todo = todos[id]; 
+  todo.done = !todo.done; 
+  todos[id] = todo;
+  localStorage.setItem('todos', JSON.stringify(todos));
+  console.log('todo state changed in local storage:', todo); 
+
+  return {todoBody, todo}; 
+}
+
+let toggleTodoUIStyle = ({todoBody, todo}) => { 
+  console.log('this is the todo that was clicked: ', todo);
+  console.log('this is the todo body: ', todoBody)
+  if (todo.done) { 
+    todoBody.style.textDecoration = 'line-through';
+  } else { 
+    todoBody.style.textDecoration = 'none';
+  }
+}
+
+
+
+let deleteFromLocalStorage = (todoIdx) => { 
+  console.log('todo to be deleted at index:', todoIdx);
+  let todos = JSON.parse(localStorage.getItem('todos')); 
+  todos.splice(todoIdx, 1); 
+  console.log('new todos after todo was deleted:', todos)
+  updateDOMElements(todos)
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+
+let updateDOMElements = (todos) => { 
+  for (i = 0; i < todos.length; i++) { 
+    todosContainer.children[i].setAttribute('name', i);
+  }
+}
+
+
+let todoDeleteButton = () => { 
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "delete"; 
+
+  deleteButton.addEventListener('click', () => { 
+    let todoIdx = deleteButton.parentElement.getAttribute('name')
+    console.log('todo index to be deleted:', todoIdx); 
+    deleteButton.parentElement.remove();
+    deleteFromLocalStorage(todoIdx)
+    
+  })
+
+  return deleteButton;
+}
+
 
 
 let makeTodoState = () => { 
@@ -30,121 +81,61 @@ let hasTodos = () => {
 
 
 let getTodosFromLocalStorage = () => { 
+  hasTodos() ? JSON.parse(localStorage.getItem('todos')).forEach((todo) => { 
+        todosContainer.appendChild(createTodo(todo)) })
+     : makeTodoState();
+}
 
-    if (hasTodos()) { 
-      JSON.parse(localStorage.getItem('todos')).forEach((todo, idx) => { 
-        //this needs to be createTodo
-        let todoHTMLElement = document.createElement("li");
-        todoHTMLElement.setAttribute('name', `${idx}`) 
-        todoHTMLElement.appendChild(todoContent(todo['content'], todo['completed']))
-         // todoHTMLElement.appendChild(todoDeleteButton()); 
-        todosContainer.appendChild(todoHTMLElement)
-      })
 
+
+let createTodo = (todo) => { 
+  let todoContainer = document.createElement("li"); 
+  let todoBody = document.createElement('p');
+  let id = todosContainer.children.length;
+
+  if (todo) { 
+    todoBody.textContent = todo.todo;
+    if (todo.done) { 
+      todoBody.style.textDecoration = 'line-through';
     } else { 
-      makeTodoState();
-    } 
+      todoBody.style.textDecoration = 'none';
+    }
+    console.log('todo created from local storage: ', todo);
 
-  console.log('local storage state on page reload:', localStorage);
+  } else { 
+    todoBody.textContent = todoInput.value;
+    addTodoToLocalStorage({todo: todoInput.value, done: false })
+  }
+  
+  todoContainer.setAttribute('name', `${id}`);
+  todoContainer.appendChild(todoBody);
+  todoContainer.appendChild(todoDeleteButton());
+  addToggleTodoHandler(todoBody);
+
+  return todoContainer;
 }
 
 getTodosFromLocalStorage()
-
-
-
-let todoContent = (text, completed) => { 
-  let todoText = document.createElement("p");
-  todoText.textContent = text; 
-  todoText.addEventListener('click', () => { 
-    console.log('completed', completed);
-  })
-
-  return todoText; 
-}
-
-
-
-
-
-
-
-//2. adding a todo 
-const createTodo = () => { 
-  let todo = document.createElement("li"); 
-  todo.appendChild(todoContent(todoInput.value));
-  todo.appendChild(todoDeleteButton());
-  let idx = todosContainer.children.length
-  todo.setAttribute('name', `${idx}`);
-
- 
-  setLocalStorage()
-  return todo;
-}
-
-
-
-
-
-//2. changing a todos state 
-//3. deleting a todo 
-//4. getting a todo from the backend
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  let setLocalStorage = () => { 
-    let length = todosContainer.children.length
-
-    console.log('inside setLocalStorage todosContainer length:', length);
-  }
-
-
-
-let todoDeleteButton = () => { 
-  let deleteButton = document.createElement("button");
-  deleteButton.textContent = "delete"; 
-
-  deleteButton.addEventListener('click', () => { 
-    let todo = deleteButton.parentElement; 
-    console.log('todo deleted id:', todo.getAttribute('name'));
-    localStorage.removeItem(todo.getAttribute('name')); 
-
-    deleteButton.parentElement.remove();
-
-    console.log('local storage state when todo is deleted:', localStorage);
-    setLocalStorage()
-  })
-
-  return deleteButton;
-}
-
-
-  
 
 todoInput.addEventListener('input', event => { 
   todoInput.value = event.target.value; 
 })
   
+
 todoButton.addEventListener('click', () => {
   todosContainer.appendChild(createTodo());
   todoInput.value = '';
 })
   
-  
+let addTodoToLocalStorage = (todo) => { 
+  let todos = JSON.parse(localStorage.getItem('todos'));
+  todos.push(todo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
 
   
   
- 
+
 
 
 
